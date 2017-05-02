2003546aa74266071aa40d824509dd9236edc27b
Kevin <kosenburg@comcast.net>
2017-05-02 10:52:15 -0400

    Add collection method for run data and excel file for graphing

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Random;
 
 /**
@@ -5,27 +8,48 @@ import java.util.Random;
  */
 public class Main {
     //TODO make graph of runs!
-    public static void main(String[] args) {
-    MinHeap heap = new MinHeap();
+    private static BufferedWriter writer;
 
-    int[] temp =  getRandomList(10);
+    public static void main(String[] args) throws IOException {
 
-    System.out.println("Insert Heap Run");
-    long runTime = System.nanoTime();
-        for (int value: temp) {
-        heap.insert(value);
+
     }
-        System.out.println("The run time is: " + (System.nanoTime() - runTime) + " ns");
-        System.out.println("Performed " + heap.getNumberOfSwaps() + " swaps");
-        heap.printInOrder();
-        System.out.println();
 
+    private static void executeRunCollections() throws IOException {
+        writer = new BufferedWriter(new FileWriter("runTimeResults"));
+
+        for (int i = 5000; i <= 50000; i += 5000 ) {
+            int[] tempList = getRandomList(i);
+            long insertTime = insertRunTime(tempList);
+            long buildTime = buildRunTie(tempList);
+
+            writer.write(i + "," + insertTime + "," + buildTime);
+            writer.newLine();
+            writer.flush();
+        }
+        writer.close();
+    }
+
+    private static long buildRunTie(int[] array) {
         System.out.println("Build Heap Run");
-        runTime = System.nanoTime();
-        heap = new MinHeap(temp);
-        System.out.println("The run time is: " + (System.nanoTime() - runTime) + " ns");
-        heap.printInOrder();
-}
+        long runTime = System.nanoTime();
+        MinHeap heap = new MinHeap(array);
+
+        return System.nanoTime() - runTime;
+    }
+
+    private static long insertRunTime(int[] array) {
+        MinHeap heap = new MinHeap();
+
+        System.out.println("Insert Heap Run");
+        long runTime = System.nanoTime();
+        for (int value: array) {
+            heap.insert(value);
+        }
+        System.out.println("Number of swaps: " + heap.getNumberOfSwaps());
+
+        return System.nanoTime() - runTime;
+    }
 
     private static int[] getRandomList(int size) {
         int[] tempList = new int[size];
@@ -33,7 +57,7 @@ public class Main {
         Random random = new Random();
 
         for(int i = 0; i < size; i++) {
-            tempList[i] = random.nextInt(100);
+            tempList[i] = random.nextInt(100000);
         }
 
         return tempList;
